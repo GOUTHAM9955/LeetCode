cd72247de0034b3a980f3360f6e14146b057e145
GOUTHAM9955 <goutham.kakani9595@gmail.com>
2025-06-04 18:48:37 -0400

    Top K Frequent Elements

--- NeetCode/src/main/java/dsa/practise/arrays_hashing/TopKFrequentElements.java
+++ NeetCode/src/main/java/dsa/practise/arrays_hashing/TopKFrequentElements.java
@@ -1,5 +1,27 @@
 package dsa.practise.arrays_hashing;
+import java.util.*;
+import java.util.HashMap;
 
 public class TopKFrequentElements {
-    
+        public int[] topKFrequent(int[] nums, int k){
+            Map<Integer, Integer> hashMap = new HashMap<>();
+            for (int num: nums){
+                hashMap.put(num,hashMap.getOrDefault(num,0)+1);
+            }
+
+            PriorityQueue<int []> priorityQueue = new PriorityQueue<>((a,b)->a[1]-b[1]);
+
+            for(Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
+                priorityQueue.offer(new int[]{entry.getKey(),entry.getValue()});
+                if(priorityQueue.size()>k){
+                    priorityQueue.poll();
+                }
+
+            }
+            int[] result = new int[k];
+            for(int i=0; i<k; i++){
+                result[i] = priorityQueue.poll()[0];
+            }
+            return result;
+        }
 }
